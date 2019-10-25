3a763daf408b3beb9a959a4a34cc491610f4f7c9
Ryan Ahern <matt.ahern@vokalinteractive.com>
2019-10-25 15:00:49 -0500

    fix: stub out basic layout for node app

---
+++ app.js
@@ -0,0 +1,70 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+// const ticketMasterRouter = require("./routes/ticketmaster");
+// const spotifyRouter = require("./routes/spotify");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  const whitelist = [];
+
+  const whitelistUrlIndex = whitelist.indexOf(req.headers.origin),
+    whitelistUrl = "";
+
+  if (whitelistUrlIndex >= 0) {
+    whitelistUrl = whitelist[whitelistUrlIndex];
+  }
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "accept, content-type, cookie");
+  res.header("Access-Control-Max-Age", "3628800");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.header("Pragma", "no-cache");
+  res.header("Expires", "0");
+
+  if (req.method == "OPTIONS") {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json(err);
+});
+
+module.exports = app;
